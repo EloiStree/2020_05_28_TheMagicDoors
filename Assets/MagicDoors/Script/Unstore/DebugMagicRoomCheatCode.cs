87f775b820ceb32859a852d043267c80462224c1
Eloi Stree <eloistree@gmail.com>
2020-06-19 00:54:08 +0200

    Save: 06/19/2020 12:54 AM

---
+++ Assets/MagicDoors/Script/Unstore/DebugMagicRoomCheatCode.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMagicRoomCheatCode : MonoBehaviour
+{
+    public Light m_defautLight;
+    public JimmyScreamer m_jimmy;
+    public PerfsDestroy m_perfDestroyer;
+
+    
+
+    public GameObject [] m_debugCanvas;
+    public GameObject m_contentRoomARef;
+    public GameObject m_contentRoomBRef;
+    public GameObject m_doorOfficalPosition;
+
+    public bool m_displayBestLoader=true;
+    public MagicDoubleDoorSwitcher m_magicSwitcher;
+    public MagicRoomLoader m_allStudentTechno;
+    public MagicRoomLoader m_bestOf;
+    public bool m_displayHand = true;
+    public GameObject [] m_handLinkedGameObject;
+
+    public void SetScreamerFPS(bool isOn)
+    {
+        m_jimmy.gameObject.SetActive(isOn);
+        for (int i = 0; i < m_debugCanvas.Length; i++)
+        {
+            m_debugCanvas[i].SetActive( isOn);
+        }
+    }
+
+    public void SpawnObjectInSceneForDebugging()
+    {
+        m_perfDestroyer.CreateAnInstance();
+    }
+
+    public void SetOnOffDoorDebugJoinHighlight()
+    {
+        SwitchGameObject(m_doorOfficalPosition);
+    }
+
+    public void SetOnOffRoomB()
+    {
+        SwitchGameObject(m_contentRoomBRef);
+    }
+
+    public void SetOnOffRoomA()
+    {
+        SwitchGameObject(m_contentRoomARef);
+    }
+
+    public void SetJimmyFPSToHight()
+    {
+        m_jimmy.m_boundaryJimmyStart = 60;
+    }
+
+    public void SetJimmyFPSToMedium()
+    {
+        m_jimmy.m_boundaryJimmyStart = 50;
+    }
+
+    public void SetJimmyFPSToLow()
+    {
+        m_jimmy.m_boundaryJimmyStart = 40;
+    }
+
+    public void LoadAnOtherRoomInZoneB()
+    {
+        m_magicSwitcher.SwitchRoomB();
+    }
+
+    public void LoadAnOtherRoomInZoneA()
+    {
+        m_magicSwitcher.SwitchRoomA();
+    }
+
+    public void SwitchLightOnOff()
+    {
+        SwitchGameObject(m_defautLight);
+    }
+
+    public void SwitchBetweenBestOfAndAll()
+    {
+        m_displayBestLoader = !m_displayBestLoader;
+        if (m_displayBestLoader)
+        {
+            SetLoadedToBestParticipants();
+        }
+        else
+        {
+            SetLoaderToAllParticipants();
+        }
+    }
+
+    public void SetLoadedToBestParticipants()
+    {
+        m_magicSwitcher.m_roomsQueue = m_bestOf.GetRoomsInShuffleQueue(true);
+        m_magicSwitcher.m_roomLoader = (m_bestOf);
+    }
+
+    public void SetLoaderToAllParticipants()
+    {
+        m_magicSwitcher.m_roomsQueue = m_allStudentTechno.GetRoomsInShuffleQueue(true);
+        m_magicSwitcher.m_roomLoader = (m_allStudentTechno);
+    }
+
+    public void SwitchHandsOnOff() {
+        SetHandOnOff(!m_displayHand);
+    }
+    public void SetHandOnOff(bool isOn)
+    {
+        m_displayHand = isOn;
+        for (int i = 0; i < m_handLinkedGameObject.Length; i++)
+        {
+            m_handLinkedGameObject[i].SetActive(isOn);
+
+        }
+       
+    }
+
+    public void SwitchJimmyScreamerAndTexts()
+    {
+        SwitchGameObject(m_jimmy);
+        for (int i = 0; i < m_debugCanvas.Length; i++)
+        {
+
+            SwitchGameObject(m_debugCanvas[i]);
+        }
+    }
+
+    public void SwitchGameObject(GameObject target) {
+        target.SetActive(!target.activeSelf);
+
+    }
+    public void SwitchGameObject(Component target) {
+        SwitchGameObject(target.gameObject);
+    }
+}
---
+++ Assets/MagicDoors/Script/Unstore/DebugMagicRoomFromKeyboard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMagicRoomFromKeyboard : MonoBehaviour
+{
+    public DebugMagicRoomCheatCode m_cheatCode;
+
+    private void Start()
+    {
+        m_cheatCode.SetScreamerFPS(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            m_cheatCode.SwitchBetweenBestOfAndAll();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            m_cheatCode.SwitchLightOnOff();
+        }
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.KeypadPeriod))
+        {
+            m_cheatCode.SwitchJimmyScreamerAndTexts();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            m_cheatCode.LoadAnOtherRoomInZoneA();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            m_cheatCode.LoadAnOtherRoomInZoneB();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            m_cheatCode.SetJimmyFPSToLow();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            m_cheatCode.SetJimmyFPSToMedium();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            m_cheatCode.SetJimmyFPSToHight();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            m_cheatCode.SetOnOffRoomA();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+        {
+            m_cheatCode.SetOnOffRoomB();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadDivide))
+        {
+            m_cheatCode.SetOnOffDoorDebugJoinHighlight();
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            m_cheatCode.SpawnObjectInSceneForDebugging();
+        }
+
+    }
+}
